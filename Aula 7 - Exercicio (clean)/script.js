ccd9adb39585c2acf846b8358a68a984b84e77e5
JulliaThainna <julliathainna@outlook.com>
2023-04-22 00:58:06 -0300

    Fixing images, making theme sound work and finish the first form

--- Aula 7 - Exercicio (clean)/script.js
+++ Aula 7 - Exercicio (clean)/script.js
@@ -11,24 +11,24 @@ function getRadio(){
 
 function changeOnClickButton(side){
   let activeImage =  document.querySelector("#carouselSW .carousel-item.active").querySelector("img");
-  let color, img;
+  let color;
 
   if(side == null){
     return;
   }
   if(side == "neutral"){
-    activeImage.setAttribute("src", "img/cat-bichinho.jpg"); 
     color = "purple";
+    song = "ewok-side"
   }
   else if(side == "dark"){
-    activeImage.setAttribute("src", "img/cat-sith.webp"); 
     color = "red";
+    song = "dark-side";
   }
   else{
-    activeImage.setAttribute("src", "img/cat-jedi.webp"); 
     color = "blue";
+    song = "light-side";
   }
-  
+
   //Esconder e remover 
   document.querySelector(".carousel-control-prev").style.visibility = "hidden";
   document.querySelector(".carousel-control-next").style.visibility = "hidden";
@@ -36,8 +36,23 @@ function changeOnClickButton(side){
   document.getElementById("button-choose").remove();
 
   //Setar estilos
+  activeImage.setAttribute("src",`img/cat-${side}.jpg`);
+  console.log(activeImage);
   document.querySelector(".swCards").style.boxShadow = `0 1rem 3rem 0.2rem ${color}`;
   document.getElementById("msg-choose-side").style.color = `${color}`;
-  document.getElementById("msg-choose-side").style.textShadow = ` 0 0 1.5rem ${color}`;
+  document.getElementById("msg-choose-side").style.textShadow = `0 0 1.5rem ${color}`;
   document.getElementById("msg-choose-side").innerHTML = `YOU CHOOSE THE ${side.toUpperCase()} SIDE!`;
+  document.getElementById("div-arrow").style.visibility = "visible";
+  document.getElementById("song-theme").innerHTML = `<audio loop id="my-audio" onloadstart="setVolAudio()"><source src="audio/${song}.mp3" type="audio/mp3"></audio>`;
+}
+
+function setVolAudio(){
+  let audio = document.getElementById("my-audio");
+  if(audio){
+    audio.play();
+    audio.volume = 0.1;
+  }
+  else{
+    alert("If you can read this it's probably because your browser doesn't support HTML media files.");
+  }
 }
